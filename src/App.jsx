3995a95d8d1f5f7ab7fa86e315b0a6f84f00e806
Thomas Aelbrecht <thomasaelbrecht@live.com>
2022-05-23 19:14:10 +0200

    Refactor to Fomantic UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import './App.css';
 import {TodoItem} from './TodoItem'
 
-const TODO_DATA = [ {text: 'create a react app', done: true, description: 'learn the basics of create-react-app'},
-{ text: 'tailwindcss', done: true, description: 'learn how to add tailwindcss to a react app'},
-{ text: 'functionality', done: false, description: 'make this do something when clicked'}]
+const TODO_DATA = [{
+  text: 'create a react app',
+  done: true,
+  description: 'learn the basics of create-react-app'
+},
+{
+  text: 'tailwindcss',
+  done: true,
+  description: 'learn how to add tailwindcss to a react app'
+},
+{
+  text: 'functionality',
+  done: false,
+  description: 'make this do something when clicked'
+}];
 
 function App() {
   return (
-    <div className="m-2">
-      <h3 className="text-lg mb-4">Todo App</h3>
-      <div className="ml-3 space-y-4">
-        {TODO_DATA.map( item => 
-          <TodoItem {...item} />
-        )}
+    <div className="ui container">
+      <div className="ui basic segment">
+        <h1 className="ui dividing header">Todo App</h1>
+        <div className="ui relaxed divided list">
+          {TODO_DATA.map( item => 
+            <TodoItem {...item} />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,22 +1,17 @@
-export function TodoItem(props) {
-  const {text, done, description } = props;
+export function TodoItem({
+  text,
+  done,
+  description
+}) {
   return (
-     <div className="relative flex items-start">
-        <div className="flex items-center h-5">
-          <input
-            type="checkbox"
-            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-            checked={done}
-          />
-        </div>
-        <div className="ml-3 text-sm">
-          <label htmlFor="comments" className="font-medium text-gray-700">
-            {text}
-          </label>
-          <p id="comments-description" className="text-gray-500">
-            {description}
-          </p>
-        </div>
-      </div>);
-
+    <div className="item">
+      <div className="ui checkbox">
+        <input type="checkbox" checked={done} />
+        <label>
+          <b>{text}</b><br/>
+          {description}
+        </label>
+      </div>
+    </div>
+  );
 }
